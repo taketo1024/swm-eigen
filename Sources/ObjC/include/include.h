2a14552f8fd4b49e0e29f2aae936af38c8945c24
Taketo Sano <taketo1024@gmail.com>
2021-05-11 21:07:38 +0900

    move headers out from include.

---
+++ Sources/ObjC/include/include.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  
+//
+//  Created by Taketo Sano on 2021/05/11.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include "../ObjCEigenDoubleMatrix.h"
+#include "../ObjCEigenIntMatrix.h"
+
+#endif /* Header_h */
